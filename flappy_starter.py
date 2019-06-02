0c3e1647b7fa46ffc60c58236fec38d6efc73c2f
Dougherty, Jeffrey (CIV) <jeffrey.dougherty@nps.edu>
2019-06-02 14:57:58 -0700

    Jessie's work done overnight, putting on sep branch until I can merge with my data structure fixes.

--- flappy_starter.py
+++ flappy_starter.py
@@ -1,10 +1,10 @@
-import os
-import sys
 import random
 import numpy as np
 from ple.games.flappybird import FlappyBird
 from ple import PLE
-from copy import deepcopy
+
+# import tensorflow as tf
+
 
 NUM_FRAMES = 10000
 
@@ -14,75 +14,83 @@ GAP = 100
 SEED = 1234
 
 N_PARAMS = 8
+N_AGENTS = 10
 
 MUTATE = True
 
-ALGO_TO_USE = 1         #0 = One that originally showed learning #1 = Jessie's  Even numbers Jessie, Odd numbers Jeff
-
-MUTATE_TO_USE = 1
+ALGO_TO_USE = 2  # 1 = Jeff,  2= Jessie's (article's)
 
 ACTION_MAP = {
     1: 119,
     0: None
 }
 
-DIST_MEAN = 0.0  # Mean of normal distribution
-DIST_SD = 0.5  # SD of normal distribution
+DIST_MEAN = .1  # Mean of normal distribution
+DIST_SD = 1     # SD of normal distribution
 
 FLAPPYBIRD = FlappyBird(width=WIDTH, height=HEIGHT, pipe_gap=GAP)
 
+
+
+
 def normalize(obs):
     x = [
-        obs['player_y']/HEIGHT,
-        obs['player_vel']/HEIGHT,
-        obs['next_pipe_dist_to_player']/WIDTH,
-        obs['next_pipe_top_y']/HEIGHT,
-        obs['next_pipe_bottom_y']/HEIGHT,
-        obs['next_next_pipe_dist_to_player']/WIDTH,
-        obs['next_next_pipe_top_y']/HEIGHT,
-        obs['next_next_pipe_bottom_y']/HEIGHT
+        obs['player_y'] / HEIGHT,
+        obs['player_vel'] / HEIGHT,
+        obs['next_pipe_dist_to_player'] / WIDTH,
+        obs['next_pipe_top_y'] / HEIGHT,
+        obs['next_pipe_bottom_y'] / HEIGHT,
+        obs['next_next_pipe_dist_to_player'] / WIDTH,
+        obs['next_next_pipe_top_y'] / HEIGHT,
+        obs['next_next_pipe_bottom_y'] / HEIGHT
     ]
 
     return np.array(x)
 
 
 def agent(x, w):
-    '''
+    """
     Perceptron agent flaps if x dot w is >= 1
 
     x is the observed state
     w is the weight vector
-    '''
+    """
     return 0 if x @ w < 0.5 else 1
 
 
-def initialize(n_agents=10):
-    '''
-    Initialize the population
-    '''
+def initialize(n_agents=N_AGENTS):
+    """
+    Population of 10 agents, each drawn from a normal distribution
+    """
 
     agents = []
 
     for i in range(n_agents):
+        # this_agent = np.zeros(8)
+        # this_agent = [1.1828843144200194, 0.047874517980502934, 0.0, -1.061810124546465, 1.0520386833146527, -0.06356522803788328, -1.061810124546465, 0.32965948666649525]
         this_agent = [np.random.normal(DIST_MEAN, DIST_SD) for j in range(N_PARAMS)]
         agents.append(this_agent)
+        # agents.append(this_agent)
 
     return agents
 
-def fitness(w, seed=SEED, headless=False):
 
-    '''Evaluate the fitness of an agent with the game
+def eval_fitness(w, seed=SEED, headless=False):
+    """
+    Evaluate the fitness of an agent with the game
     game is a PLE game
-    agent is an agent function'''
+    agent is an agent function
+    """
 
     # disable rendering if headless
     if headless:
-        display_screen=False
-        force_fps=True
+        display_screen = False
+        force_fps = True
     else:
-        display_screen=True
-        force_fps=False
+        display_screen = True
+        force_fps = False
 
+    # game init
     game = PLE(FLAPPYBIRD, display_screen=display_screen, force_fps=force_fps, rng=seed)
     game.init()
     game.reset_game()
@@ -90,177 +98,254 @@ def fitness(w, seed=SEED, headless=False):
 
     agent_score = 0
     r_vals = []
+    dist_traveled = 0
 
     while True:
         if game.game_over():
             break
 
         obs = game.getGameState()
+        # print(obs)
         x = normalize(obs)
         action = agent(x, w)
 
         reward = game.act(ACTION_MAP[action])
 
-        #Score: Pipes traversed * r, see notes
-        # agent_score += 1
-        if ALGO_TO_USE == 0:
-            #Jeff n' Jessie v1.0
-            agent_score += 1  # Number of frames traveled
-            safe_zone_width = x[3] - x[4]
-            safe_zone_center = safe_zone_width / 2
-            r_vals.append((safe_zone_width - abs(safe_zone_center - x[0])) / safe_zone_width)
-        elif ALGO_TO_USE == 1:
-            #Jessie's revision to our algorithm
+        # Score: Pipes traversed * r, see notes
+        if ALGO_TO_USE == 1:
             center = abs(obs['next_pipe_top_y'] - obs['next_pipe_bottom_y'] / 2)
             target = obs['next_pipe_top_y'] + center
-            player_success = 1 - abs(target - obs['player_y'])/target
-            r_vals.append(player_success)
-        elif ALGO_TO_USE == 3:
-            #Chancho's algorithm
-            if reward > 0:
-                agent_score += 1
-            agent_score += x[2]
 
-    if ALGO_TO_USE == 0:
-        agent_score = agent_score * np.mean(r_vals)
-    elif ALGO_TO_USE == 1:
+            player_success = 1 - abs(target - obs['player_y']) / target
+            r_vals.append(player_success)
+
+
+        # distance traveled - next_pipe_dist_to_player
+        elif ALGO_TO_USE == 2:
+
+            if reward >= 0:
+                # next_pipe_dist_to_player[t - 1] - next_pipe_dist_to_player[t] = 4
+                dist_traveled += 4
+                # (this is consistent throughout game)
+
+    # agent's fitness
+
+    if ALGO_TO_USE == 1:
+
         agent_score = np.mean(r_vals)
-    # agent_score = agent_score * np.mean(r_vals)
+
+
+    elif ALGO_TO_USE == 2:
+        agent_score = dist_traveled - obs['next_pipe_dist_to_player']
+
     return agent_score
 
-def crossover(w1, w2):
-    '''
-    Generate an offspring from two agents
-    '''
-    crossover_pt = random.randint(0, N_PARAMS-1)    #Each agent is N_PARAMS long
 
-    #Complying with directions to do single crossover
+def crossover(parents):
+    """
+    Single-Point Crossover:
+    Generate an offspring from two agents, w1 and w2
+    """
+    w1 = parents[0]
+    w2 = parents[1]
+
+    # Each agent is N_PARAMS long
+
+    crossover_pt = random.randint(0, N_PARAMS - 1)
     swap = w1[crossover_pt]
     w1[crossover_pt] = w2[crossover_pt]
     w2[crossover_pt] = swap
 
-    #Cross over everything from there to end
-    #for i in range(crossover_pt, N_PARAMS):
-    #    swap = w1[i]
-    #    w1[i] = w2[i]
-    #    w2[i] = swap
+    # the chosen one
+    chosen_one = [w1, w2][random.randint(0, 1)]
 
-    return [w1, w2]
+    return [chosen_one]
 
 
 def mutate(w):
-    '''
+    """
     Apply random mutations to an agent's genome
-    '''
-    #Given in assignment p(mutate) should be 0.5
-    #Rather than mess around with floats, just flip a coin
-    mut = random.randint(0,1)
-    if MUTATE_TO_USE == 0:
-        if mut == 1:        #One means yes, mutate!  Binary joke, ga-harf, ga-harf
-            #First pass at mutation: pick one position in array and randomize.
-            mut_posit = random.randint(0, N_PARAMS-1)
-            w[mut_posit] = np.random.normal(DIST_MEAN, DIST_SD)
-    elif MUTATE_TO_USE == 1:
-        #Chancho's mutate algorithm, add a small variation at all positions rather than just randomizing one position
-        if mut == 1:
-            for i in range(N_PARAMS):
-                a = np.random.normal(DIST_MEAN, (DIST_SD/2))
-                w[i] += a
+    """
+    # Given in assignment p(mutate) should be 0.5
 
-        #Other possibilities: generate small random quantity around mean of 0, add to one/each position
+    # Hit a ton of plateaus, wanted to introduce a different kind of disruption
+    mut = random.randint(0, 1)
+
+    # if mut == 1:  # One means yes, mutate!  Binary joke, ga-harf, ga-harf
+        # First pass at mutation: pick one position in array and randomize.
+    # mut_posit = random.randint(0, N_PARAMS - 1)
+    # w[mut_posit] = np.random.normal(DIST_MEAN, DIST_SD)
+
+    # else:
+        # add noise to all weights
+    noise = np.random.normal(DIST_MEAN, DIST_SD, 8)
+    w += noise
+
+    # Other possibilities: generate small random quantity around mean of 0, add to one/each position
     return w
 
 
-def train_agent(n_agents=20, n_epochs=100, headless=True):
-    '''
+def train_agent(n_agents=N_AGENTS, n_epochs=1000, headless=False):
+    """
     Train a flappy bird using a genetic algorithm
-    '''
-    #
+    """
     # TODO: genetic algorithm steps below
+
+    # manually picked best weights thus far
+    # superstar = [
+    #     [1.1828843144200194, 0.047874517980502934, 0.0, -1.061810124546465, 1.0520386833146527, -0.06356522803788328,
+    #      -1.061810124546465, 0.32965948666649525]]
+    # superstar = superstar[0]
 
     res = open('training_results.txt', 'w')
 
-    # initialization
+    # --Initialization--
+    # population = initialize(n_agents - 1)
     population = initialize(n_agents)
+    # population.append(superstar)
 
-    top_2 = []          #Defining here so PyCharm will shut up
+    best_weights = []
+    best_fitness = 0
 
     for g in range(n_epochs):
-        # evaluate fitness
+
+        # to evaluate fitness
         agent_dict = {}
         fit_list = []
-        top_2 = []
-        parents = []
+        winners = []
+        children = []
 
-        for i in range(len(population)):                        #Want both a way to sort fitness scores and a quick way to find agent associated with that fitness score
-            #Note: this approach could have problems if we get agents with identical fitness.  Cross that bridge if we come to it.
-            w = population[i]
-            w_fit = fitness(w, headless=headless)   #Fitness for this agent
+        # Want both a way to sort fitness scores and a quick way
+        # to find agent associated with that fitness score
+        for w in population:
+            # Fitness for this agent
+
+            w_fit = eval_fitness(w, headless=headless)
             fit_list.append(w_fit)
+            print(w_fit, w)
 
-            if w_fit not in agent_dict:             #No longer assuming that fitnesses are unique, since they apparently aren't
+            # one measure of fitness could have multiple
+            # sets of weights associated with it
+
+            if w_fit not in agent_dict:
                 agent_dict[w_fit] = []
+
             agent_dict[w_fit].append(w)
 
-        fit_list.sort(reverse=True)                             #Put fitness list in order
+            # print('consistent best fitness:', best_fitness ==)
+            # print('consistent best weights:', best_weights ==)
 
-        for i in range(4):
-            this_fitness = fit_list[i]
-            this_fitness_list = agent_dict[this_fitness]
-            while len(this_fitness_list) > 0 and len(top_2) < 2 and len(parents) < 4:
-                if len(this_fitness_list) > 1:
-                    random.shuffle(this_fitness_list)
-                this_agent = this_fitness_list.pop()
-                top_2.append(this_agent)
-                parents.append(this_agent)
-            while len(this_fitness_list) > 0 and len(parents) < 4:
-                if len(this_fitness_list) > 1:
-                    random.shuffle(this_fitness_list)
-                parents.append(this_fitness_list.pop())
+            # verify best fitness & associated weights
+
+            if best_fitness < w_fit:
+                best_fitness = w_fit
+                best_weights = agent_dict[w_fit][:]
+                print('best_fitness', best_fitness)
+                print('best_weights', best_weights)
+                res.write('\n' + '*FITNESS  ' + str(best_fitness) + '\n\n')
+                res.write('\n' + '*WEIGHTS' + '\n' + str(best_weights) + '\n\n')
+
+            res.write(str(w) + '\n' + str(w_fit) + '\n')
+
+        fit_list.sort(reverse=True)  # Put fitness list in order
+
+        # -- Selection --
+        # Choose the 4 best agents for mating = "winners"
+        best = 0
+        count = 0
+
+        while count < 4:
+
+            # grab top agent metrics
+            fitness = fit_list[best]
+            weight_list = agent_dict[fitness][:]
+
+            ### CHECK weight_list len
+
+            random.shuffle(weight_list)
+
+            # when we have multiple sets per fitness
+            while len(weight_list) > 0 and len(winners) != 4:
+                choice = weight_list[0][:]
+                weight_list = weight_list[1:][:]
+
+                winners.append(choice)
+
+                count += 1
+
+            best += 1
+
+        top_2 = winners[:2]
+
+        # -- Crossover --
+        # Create 6 children total from the winner pool
+        # * 1 child of top 2 winners
+        # * 2 direct clones of top 2 winners
+        # * 3 offspring of randomly chosen 2 of the 4 winners
+
+        # Clones for the top 2 - not subject to mutation
+        clones = [i[:] for i in top_2]
+        # print('clones==top_2', clones == top_2)
+
+        # Children of the top 2
+        children += crossover([top_2[0], top_2[1]]) + clones
+
+        # creates 3 offspring
+
+        for _ in range(3):
+            random.shuffle(winners)
+            children += crossover([winners[0], winners[1]])
+
+        # -- Mutation --
+        offspring = []
+
+        for c in range(len(children)):
+
+            if random.randint(0, 1):
+
+                mutant = mutate(children[c])
+
+            else:
+
+                mutant = children[c]
+
+            offspring.append(mutant)
 
 
-        # crossover
-        #children = [deepcopy(i) for i in top_2]
-        clones = [deepcopy(i) for i in top_2] #Clones for the top 2, will not be subjected to mutation
-        children = []
-        children = children + crossover(top_2[0], top_2[1])  #Children of the top 2
-        for _ in range(n_agents - 4):
-            '''parents = np.random.choice(winners_fitness, 2, False)       #Choose two different winners at random from the top 4
-            children += crossover(agent_dict[parents[0]], agent_dict[parents[1]])'''
-            random.shuffle(parents)
-            children = children + crossover(parents[0], parents[1])
+        # -- Insertion --
+        population = clones + winners + offspring
 
 
+        print('consistent best weights:', best_weights[0] == population[0])
+        print(len(best_weights[0]) == len(population[0]))
+        print('best_weights', best_weights)
 
-        # mutation
-        if MUTATE:
-            children = [mutate(_) for _ in children]
 
-        # insertion
-        population = children
-        population = population + clones
-
-        result_string = "Generation " + str(g) +" Best Score: " + str(fit_list[0]) + "\n"
+        result_string = "Generation " + str(g) + " Best Score: " + str(fit_list[0]) + "\n"
 
         print(result_string)
         res.write(result_string)
 
-    # return the best agent found
-    best_agent = top_2[0]       #The top agent of the last generation to undergo fitness evaluation
+    # RETURN: The top agent of the last generation to undergo fitness evaluation
+    best_agent = top_2[0]
+    print('best_agent', best_agent)
+    print('consistency:', best_weights[0] == best_agent)
+
+    
     res.close()
     return best_agent
 
 
-def main(w, seed=SEED, headless=True):
-    '''
+def main(w, seed=SEED, headless=False):
+    """
     Let an agent play flappy bird
-    '''
+    """
     if headless:
-        display_screen=False
-        force_fps=True
+        display_screen = False
+        force_fps = True
     else:
-        display_screen=True
-        force_fps=False
+        display_screen = True
+        force_fps = False
 
     game = PLE(FLAPPYBIRD, display_screen=display_screen, force_fps=force_fps, rng=seed)
     game.init()
@@ -274,7 +359,9 @@ def main(w, seed=SEED, headless=True):
         if game.game_over():
             break
 
-        x = normalize(game.getGameState())
+        obs = game.getGameState()
+        x = normalize(obs)
+        # print(obs)
         action = agent(x, w)
 
         reward = game.act(ACTION_MAP[action])
@@ -289,11 +376,18 @@ def main(w, seed=SEED, headless=True):
 
 
 if __name__ == '__main__':
-    human_play = False
+    human_play = 0
+
+    # added bc population reaches equilibrium v fast (aka stops learning & can't improve)
+
+    # np.random.seed(random.randint(0, 100))
     np.random.seed(1234)
+
     if not human_play:
         w = train_agent()
+        print(w)
         main(w)
+
     else:
-    # For human play, use 'W' key to flap
+        # For human play, use 'W' key to flap
         main(np.zeros(8), headless=False)
